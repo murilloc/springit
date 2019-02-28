316e710650064f6c23bea01576700786b42639ee
Murillo <murilloc@gmail.com>
2019-02-28 16:59:33 -0300

    Prepared to start a new section: security

--- src/main/java/br/nocclarobrasil/springit/SpringitApplication.java
+++ src/main/java/br/nocclarobrasil/springit/SpringitApplication.java
@@ -1,35 +1,27 @@
 package br.nocclarobrasil.springit;
 
-import br.nocclarobrasil.springit.domain.Comment;
-import br.nocclarobrasil.springit.domain.Link;
-import br.nocclarobrasil.springit.repository.CommentRepository;
-import br.nocclarobrasil.springit.repository.LinkRepository;
-import org.springframework.boot.CommandLineRunner;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringitApplication {
     
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+    
     public static void main(String[] args) {
+        
         SpringApplication.run(SpringitApplication.class, args);
     }
     
-    //@Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-        return args -> {
-            Link link = new Link("Getting started with Soring Boot 2", "https://threrealdanvega.com/");
-            linkRepository.save(link);
-            
-            Comment comment = new Comment("This Spring Boot 2 link is awesome", link);
-            commentRepository.save(comment);
-            
-            link.getComments().add(comment);
-            
-            
-        };
+    @Bean
+    PrettyTime prettyTime() {
+        return new PrettyTime();
     }
     
 }
--- src/main/java/br/nocclarobrasil/springit/domain/Link.java
+++ src/main/java/br/nocclarobrasil/springit/domain/Link.java
@@ -1,15 +1,24 @@
 package br.nocclarobrasil.springit.domain;
 
+import br.nocclarobrasil.springit.service.BeanUtil;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -23,12 +32,30 @@ public class Link extends Auditable {
     private Long id;
     
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
     
-    @lombok.NonNull
+    @NonNull
+    @NotEmpty(message = "Please enter an url.")
+    @URL(message = "Please enter a valid URL")
     private String url;
     
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
+    
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+    
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+    
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
     
 }
